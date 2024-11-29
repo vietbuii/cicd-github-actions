54c468551d0eb50e5d8377fbee1dd6f1ec567f36
vietbuii <vietbh.26@gmail.com>
2024-11-30 01:21:07 +0700

    code

---
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Zit!";
+	}
+
+//	echo "# cicd-github-actions" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/vietbuii/cicd-github-actions.git
+//	git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
