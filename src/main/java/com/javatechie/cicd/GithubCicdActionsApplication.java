7c48896d4957ef54c6bcac2926c85fcefb0c0b28
vietbuii <vietbh.26@gmail.com>
2024-12-14 04:12:52 +0700

    fix index file

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -12,7 +12,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to Zit!!";
+		return "Welcome to my website!";
 	}
 
 	@GetMapping("/goodbye")
